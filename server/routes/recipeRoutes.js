1628da90c77428b2eced311692d35a37e81e8874
Jamshid <kalandarovjamshid01@gmail.com>
2022-07-08 17:04:10 +0500

    okay

--- server/routes/recipeRoutes.js
+++ server/routes/recipeRoutes.js
@@ -12,5 +12,5 @@ router.post("/search", recipeController.searchRecipe);
 router.get("/explore-latest", recipeController.exploreLatest);
 router.get("/explore-random", recipeController.exploreRandom);
 router.get("/submit-recipe", recipeController.submitRecipe);
-router.get("/submit-recipe", recipeController.submitRecipeOnPost);
+router.post("/submit-recipe", recipeController.submitRecipeOnPost);
 module.exports = router;
